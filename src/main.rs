49ae3e5cd0ec673227819a1c5f36678dbb3d5239
Andrew Pipo <93549755+PipoAT@users.noreply.github.com>
2024-07-02 08:13:08 -0400

    Added example .txt file and ability to read files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     // get/accept arguments from the command line
@@ -10,4 +11,11 @@ fn main() {
     // print out the saved variables
     println!("Searching for {query}");
     println!("In file {file_path}");
+
+    // read the file
+    let contents = fs::read_to_string(file_path)
+        .expect("Something went wrong reading the file");
+
+    // print out the file contents
+    println!("With text:\n{}", contents);
 }
