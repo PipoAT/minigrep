f4f871ddfc95f6fb9ecf182023e0d1cf75238916
Andrew Pipo <93549755+PipoAT@users.noreply.github.com>
2024-07-02 08:09:52 -0400

    Added command line argument acceptance

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // get/accept arguments from the command line
+    let args: Vec<String> = env::args().collect();
+    // save argument values into variables
+    let query = &args[1];
+    let file_path = &args[2];
+
+    // print out the saved variables
+    println!("Searching for {query}");
+    println!("In file {file_path}");
 }
