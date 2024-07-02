4cf9b90e10d57e96effb9d664f4aa56b4b97fb5c
Andrew Pipo <93549755+PipoAT@users.noreply.github.com>
2024-07-02 08:51:54 -0400

    Writing error and output to text file

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,15 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    // get/accept arguments from the command line
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
